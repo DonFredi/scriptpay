d696f4a1abbbf594414815bcf2d053c312034f2f
DonFredi <dondadi.fn@gmail.com>
2026-05-31 22:12:42 +0300

    second commit

--- src/modules/api-docs/sections/Overview.tsx
+++ src/modules/api-docs/sections/Overview.tsx
@@ -5,7 +5,7 @@ const Overview = () => {
     <SectionWrapper>
       <h1>M-Pesa API documentation</h1>
       <h2>Overview</h2>
-      <p>Use this API to integrate M-Pesa payments into your application.</p>
+      <p>Use this API to integrate M-Pesa payments into your application</p>
       <h2>Base URL</h2>
       <div className="bg-gray-100 p-3 rounded mt-2 text-sm">https://mpesa-payments.vercel.app/api/mpesa</div>
 
