97d4c04546e47e0dd9811a88c8b3f525baf810df
DonFredi <dondadi.fn@gmail.com>
2026-06-18 14:03:11 +0300

    fix console

--- src/modules/console/ConsolePage.tsx
+++ src/modules/console/ConsolePage.tsx
@@ -1,14 +1,12 @@
 import PageWrapper from "@/shared/components/shared/PageWrapper";
-import RequestSection from "./sections/RequestSection";
+// import RequestSection from "./sections/RequestSection";
 
 const ConsolePage = () => {
   return (
     <PageWrapper>
       <h1>Test Console</h1>
       <p>Test API endpoints and view responses in real-time.</p>
-      <div>
-        <RequestSection />
-      </div>
+      <div>{/* <RequestSection /> */}</div>
     </PageWrapper>
   );
 };
