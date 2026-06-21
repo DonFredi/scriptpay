0220cadc0ead860fc81b3956074ab6c50d686c39
DonFredi <dondadi.fn@gmail.com>
2026-06-21 14:29:24 +0300

    dashboardpage fix

--- src/app/(main)/(protected)/admin/dashboard/page.tsx
+++ src/app/(main)/(protected)/admin/dashboard/page.tsx
@@ -1,13 +1,7 @@
-import PageHeading from "@/shared/components/shared/PageHeading";
-import PageWrapper from "@/shared/components/shared/PageWrapper";
-import SectionWrapper from "@/shared/components/shared/SectionWrapper";
-
 export default function DashboardPage() {
   return (
-    <PageWrapper>
-      <SectionWrapper>
-        <PageHeading>Dashboard Page</PageHeading>
-      </SectionWrapper>
-    </PageWrapper>
+    <div>
+      <DashboardPage />
+    </div>
   );
 }
